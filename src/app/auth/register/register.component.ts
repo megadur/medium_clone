8a8b2cfde15866a5cde74b24c634bcf517a93628
Mega Dur <megadur@gmail.com>
2024-09-28 16:59:37 +0200

    init

---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [RouterLink, ReactiveFormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  form: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.nonNullable.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    })
+
+  }
+onNgInit() {
+
+}
+  onSubmit() {
+    console.log('form', this.form!.getRawValue())
+  }
+}
